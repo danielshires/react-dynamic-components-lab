a8a6edb7c6008add06048e68741bfcfbaadb5a33
danielshires <hello@danielshires.com>
2022-06-08 20:56:57 +1000

    Assignment complete

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 export default class ColorBox extends Component {
 
   state = {
@@ -9,12 +10,49 @@ export default class ColorBox extends Component {
   }
 
   render() {
-    return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+
+    const returnValue = this.props.opacity - 0.1
+
+    return (  
+      <div className="color-box" style={{ opacity: this.props.opacity }}>
+        {this.props.opacity <= 0.2 ? null : <ColorBox opacity={returnValue}/>}
       </div>
     )
   }
-
+  // render() {
+  //   const returnValue = this.props.opacity - 0.1
+  //   return this.props.opacity > 0.2 ? null : (
+  //     <div className="color-box" style={{ opacity: this.props.opacity }}>
+  //       <ColorBox opacity={returnValue}/>
+  //     </div>
+  //   )
+  // }
 }
 
+
+// XI
+
+// export default class Example extends Component {
+//   render() {
+//     const newValue = this.props.value * 2;
+//     return this.props.value > 100 ? null : ( <div>
+//         <Example value={newValue} />
+//       </div>)
+//   }
+// }
+
+// NICK
+
+// render() {
+//   const returnValue = this.props.opacity - 0.1
+//   return this.props.opacity > 0.2 ? null : ( 
+//   <div className='color-box' style={{ opacity: this.props.opacity}}>
+//    <ColorBox opacity={returnValue}/>
+//   </div>)
+    
+    // <div className="color-box" style={{opacity: this.props.opacity}}>
+    //   {
+    //   <ColorBox style={{opacity: this.props.opacity}}/>
+    //   }
+    // </div>
+// }
